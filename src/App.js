ae9c8c81a95c6a0f630c0dba4456a883b310c2e5
Moze <mosesibe@gmail.com>
2020-04-09 14:40:34 +0100

    adding header somponents

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 
-const Hatspage = () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import './App.css';
+import ShopPage from './pages/shop/shop.component';
+import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={Hatspage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
