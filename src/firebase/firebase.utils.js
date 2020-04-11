98f5c86822e0587efc35ff23dc86987bf7424c51
Moze <mosesibe@gmail.com>
2020-04-12 00:37:31 +0100

    impleneted firebase utill abd handles auth to datatbase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,19 +11,46 @@ const config ={
     messagingSenderId: "116907425927",
     appId: "1:116907425927:web:e5f34ace9cf08c75d6b822",
     measurementId: "G-4D5ZXZGC7P"
-  }
+  };
+
 
   firebase.initializeApp(config);
 
+  // UserAuth is passed when a successful signin  happens
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return; // if user dont exit just return
+  
+    // Search and retrieve the User collection for the user with docID of userAuth.uid
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+    // Snapshot represent the object no the actuall data
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      // Async action to store the authenticated user in the user collection with .set()
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    // We return the UserRef object to put in the currentUser state for further identification in the app
+    return userRef;
+  };
+  
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
-
-  // Access to google auth provider class fron the authentication library
-  const provider = new firebase.auth.GoogleAuthProvider();
-  // We want trigger the goole pop up when ever we click sign in with Google
-  provider.setCustomParameters({ prompt: 'select_account'});
-
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
-  export default firebase;
   
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
+  export default firebase;
